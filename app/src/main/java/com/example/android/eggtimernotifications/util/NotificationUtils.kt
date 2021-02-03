8c3235d935207e6c1c0ed35b28ac448477cfdd14
koknaev <89024464050@yandex.ru>
2021-02-03 19:53:44 +0500

    part 12 done

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -38,42 +38,45 @@ private val FLAGS = 0
  * @param context, activity context.
  */
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
-    // Create the content intent for the notification, which launches
-    // this activity
-    // TODO: Step 1.11 create intent
 
-    // TODO: Step 1.12 create PendingIntent
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
 
-    // TODO: Step 2.0 add style
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
+    val eggImage = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.cooked_egg)
+
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(eggImage)
+        .bigLargeIcon(null)
     // TODO: Step 2.2 add snooze action
-
-    // TODO: Step 1.2 get an instance of NotificationCompat.Builder
-    // Build the notification
 
     val builder = NotificationCompat.Builder(
         applicationContext,
         applicationContext.getString(R.string.egg_notification_channel_id)
     )
 
-    // TODO: Step 1.8 use the new 'breakfast' notification channel
-
-    // TODO: Step 1.3 set title, text and icon to builder
-
         .setSmallIcon(R.drawable.cooked_egg)
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
-    // TODO: Step 1.13 set content intent
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
+        .setStyle(bigPicStyle)
+        .setLargeIcon(eggImage)
+    // TODO: Step 2.3 add snooze action
 
-        // TODO: Step 2.1 add style to builder
-
-        // TODO: Step 2.3 add snooze action
-
-        // TODO: Step 2.5 set priority
+    // TODO: Step 2.5 set priority
 
     // TODO: Step 1.4 call notify
     notify(NOTIFICATION_ID, builder.build())
 
 }
 
-// TODO: Step 1.14 Cancel all notifications
+
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
